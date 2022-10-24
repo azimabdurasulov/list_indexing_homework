ce2e042db247af6ddfc92d7323acbc3255b8d0f5
azimabdurasulov <azimjon20050604@gmail.com>
2022-10-24 18:40:26 +0500

    List08.py

--- List08.py
+++ List08.py
@@ -6,4 +6,11 @@ def main(list1):
     Returns:
         list: return answer
     """
-    return
+    i = 0
+    while i < len(list1):
+        list1[i] = bool(list1[i])
+        
+        i += 1
+    return list1
+
+print(main([1, 0, 0, 1, 1]))
